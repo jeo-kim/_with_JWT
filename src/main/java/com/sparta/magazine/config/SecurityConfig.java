762e7483840c9ac6b3ddd27739793109c6c1f516
jeonghyeon <skjh0807s@snu.ac.kr>
2022-02-25 20:09:37 +0900

    postController: 로그인식별,400에러메시지 ok

--- src/main/java/com/sparta/magazine/config/SecurityConfig.java
+++ src/main/java/com/sparta/magazine/config/SecurityConfig.java
@@ -80,6 +80,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/authenticate").permitAll()
                 .antMatchers("/api/signup").permitAll()
                 .antMatchers("/api/post").permitAll()
+                .antMatchers("/api/post/**").permitAll()
 
                 .anyRequest().authenticated()
 
