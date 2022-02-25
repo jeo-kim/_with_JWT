ff60bacd2bcc394b597b43dafcb419ae7e67c05f
jeonghyeon <skjh0807s@snu.ac.kr>
2022-02-25 18:39:46 +0900

    jwt

---
+++ src/main/java/com/sparta/magazine/dto/TokenDto.java
@@ -0,0 +1,14 @@
+package com.sparta.magazine.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+}
---
+++ src/main/java/com/sparta/magazine/service/UserService.java
@@ -0,0 +1,60 @@
+package com.sparta.magazine.service;
+
+
+import java.util.Collections;
+import java.util.Optional;
+import com.sparta.magazine.dto.UserDto;
+import com.sparta.magazine.entity.Authority;
+import com.sparta.magazine.entity.User;
+//import com.sparta.magazine.exception.DuplicateMemberException;
+import com.sparta.magazine.repository.UserRepository;
+import com.sparta.magazine.util.SecurityUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public User signup(UserDto userDto) {
+        if (userRepository.findOneWithAuthoritiesByUsername(userDto.getUsername()).orElse(null) != null) {
+            throw new RuntimeException("이미 가입되어 있는 유저입니다.");
+        }
+
+        Authority authority = Authority.builder()
+                .authorityName("ROLE_USER")
+                .build();
+
+        User user = User.builder()
+                .username(userDto.getUsername())
+                .password(passwordEncoder.encode(userDto.getPassword()))
+                .nickname(userDto.getNickname())
+                .authorities(Collections.singleton(authority))
+                .activated(true)
+                .build();
+
+//        return UserDto.from(userRepository.save(user));
+        return userRepository.save(user);
+
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<User> getUserWithAuthorities(String username) {
+//        return UserDto.from(userRepository.findOneWithAuthoritiesByUsername(username).orElse(null));
+        return userRepository.findOneWithAuthoritiesByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<User> getMyUserWithAuthorities() {
+//        return UserDto.from(SecurityUtil.getCurrentUsername().flatMap(userRepository::findOneWithAuthoritiesByUsername).orElse(null));
+        return SecurityUtil.getCurrentUsername().flatMap(userRepository::findOneWithAuthoritiesByUsername);
+    }
+}
